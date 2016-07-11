f52cee4957506e23c7aaf88256ea48bdfd01e8f1
Aksi0m <returic@gmail.com>
2016-07-11 10:30:26 +0200

    Update NetworkManager.java

--- app/src/main/java/aksiom/currencycalc/support/NetworkManager.java
+++ app/src/main/java/aksiom/currencycalc/support/NetworkManager.java
@@ -15,17 +15,6 @@ public class NetworkManager {
         this.context = context;
     }
 
-    /*public static boolean isConnected() {
-        Runtime runtime = Runtime.getRuntime();
-        try {
-            Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
-            int     exitValue = ipProcess.waitFor();
-            return (exitValue == 0);
-        } catch (IOException e)          { e.printStackTrace(); }
-        catch (InterruptedException e) { e.printStackTrace(); }
-        return true;
-    }*/
-
     public boolean isConnected(){
         ConnectivityManager cm =
                 (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
